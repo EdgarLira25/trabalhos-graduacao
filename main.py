4a07208bf97ebfa6eb12dd219c627f01ff72b60e
Edgar Lira <edgar.jj.29@gmail.com>
2024-09-01 01:45:07 -0300

    feat: habilita CORS origin *

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 "Inicializa FLASK API"
 from flask import Flask
-from api.routes.movies import movies_blueprint
+from flask_cors import CORS
+from api.routes.query import query
 
 if __name__ == "__main__":
 
     app = Flask(__name__)
-    app.register_blueprint(movies_blueprint)
+    CORS(app, origins="*")
+    # app.register_blueprint(movies_blueprint)
+    app.register_blueprint(query)
     app.run(port=8000, debug=False)
